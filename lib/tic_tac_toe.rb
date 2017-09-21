92b7be1097178f8ac76480d384acc2d2b6046e66
John Tuttle <thejwt@gmail.com>
2017-09-21 19:42:33 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,7 +34,7 @@ class TicTacToe
     input = gets.strip
     input_to_index(input)
     if valid_move?()
-        current_player
+        current_player()
         move()
         display_board()
     else
